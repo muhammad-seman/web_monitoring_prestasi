351451798d059f56cbaaff2f759f7e21c69acad6
sebehhhh <muhammad.seman030801@gmail.com>
2025-07-03 14:58:43 +0800

    penyesuaian

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Login Sistem Informasi Akademik MAPK NU Haruyan</title>
+  <title>Login </title>
   <link rel="shortcut icon" type="image/png" href="{{ asset('assets/images/logos/favicon.png') }}" />
   <link rel="stylesheet" href="{{ asset('assets/css/styles.min.css') }}" />
 
